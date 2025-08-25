6b19a377a5565a6a24a0246388d49c1bb9d367a1
Wilber Martinez <wilber@wmartin8@gmail.com>
2025-08-25 08:40:24 -0400

    add keymap for floating error message

--- init.lua
+++ init.lua
@@ -180,6 +180,7 @@ vim.opt.autoindent = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
